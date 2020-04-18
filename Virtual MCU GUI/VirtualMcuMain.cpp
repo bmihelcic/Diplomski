eb2755032d4037b0d9ab58366df271d9bcece331
Branimir Mihelčić <bran1mir.bm@gmail.com>
2020-04-18 21:18:02 +0200

    Implemented sliders for setting values on two ADC channels

--- Virtual MCU GUI/VirtualMcuMain.cpp
+++ Virtual MCU GUI/VirtualMcuMain.cpp
@@ -9,44 +9,62 @@ wxBEGIN_EVENT_TABLE(VirtualMcuMain, wxFrame)
 	EVT_BUTTON(ID_input_1, OnButtonClick)
 	EVT_BUTTON(ID_input_2, OnButtonClick)
 	EVT_BUTTON(ID_input_3, OnButtonClick)
+	EVT_SLIDER(ID_slider_0, OnSliderUpdate)
+	EVT_SLIDER(ID_slider_1, OnSliderUpdate)
 wxEND_EVENT_TABLE()
 
 // main frame doesn't have any parent, ID can be any, and at last give it a title
 VirtualMcuMain::VirtualMcuMain() : wxFrame(nullptr, wxID_ANY, "VIRTUAL MCU")
 {
 	this->SetSizeHints(500, 250);
+	this->SetBackgroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_BTNHIGHLIGHT));
 
 	wxBoxSizer* wx_box_sizer_0;
 	wx_box_sizer_0 = new wxBoxSizer(wxVERTICAL);
 
 	wxBoxSizer* wx_box_sizer_1;
-	wx_box_sizer_1 = new wxBoxSizer(wxVERTICAL);
-	wxBoxSizer* wx_box_sizer_1_1;
-	wx_box_sizer_1_1 = new wxBoxSizer(wxHORIZONTAL);
-	wxGridSizer* wx_grid_sizer_1_1_1;
-	wx_grid_sizer_1_1_1 = new wxGridSizer(0, 4, 0, 0);
-	wxGridSizer* wx_grid_sizer_1_1_2;
-	wx_grid_sizer_1_1_2 = new wxGridSizer(0, 4, 0, 0);
-	wxBoxSizer* wx_box_sizer_1_2;
-	wx_box_sizer_1_2 = new wxBoxSizer(wxVERTICAL);
+	wx_box_sizer_1 = new wxBoxSizer(wxHORIZONTAL);
 
+	wxFlexGridSizer* wx_flex_grid_sizer_1_1;
+	wx_flex_grid_sizer_1_1 = new wxFlexGridSizer(0, 4, 0, 0);
+	wx_flex_grid_sizer_1_1->SetFlexibleDirection(wxBOTH);
+	for (int i = 0; i < 4; i++) { wx_flex_grid_sizer_1_1->AddGrowableCol(i);}
+	//wx_flex_grid_sizer_1_1->AddGrowableRow(1);	
+	wx_flex_grid_sizer_1_1->SetNonFlexibleGrowMode(wxFLEX_GROWMODE_SPECIFIED);
+
+	wxFlexGridSizer* wx_flex_grid_sizer_1_2;
+	wx_flex_grid_sizer_1_2 = new wxFlexGridSizer(0, 4, 0, 0);
+	wx_flex_grid_sizer_1_2->SetFlexibleDirection(wxBOTH);
+	for (int i = 0; i < 4; i++) { wx_flex_grid_sizer_1_2->AddGrowableCol(i); }
+	//wx_flex_grid_sizer_1_2->AddGrowableRow(1);	
+	wx_flex_grid_sizer_1_2->SetNonFlexibleGrowMode(wxFLEX_GROWMODE_SPECIFIED);
+	
 	wxBoxSizer* wx_box_sizer_2;
-	wx_box_sizer_2 = new wxBoxSizer(wxVERTICAL);
+	wx_box_sizer_2 = new wxBoxSizer(wxHORIZONTAL);
+
+	wxFlexGridSizer* wx_flex_grid_sizer_2_1;
+	wx_flex_grid_sizer_2_1 = new wxFlexGridSizer(0, 3, 0, 0);
+	//for (int i = 0; i < 3; i++) { wx_flex_grid_sizer_2_1->AddGrowableCol(i); }
+	wx_flex_grid_sizer_2_1->AddGrowableCol(1);	
+	wx_flex_grid_sizer_2_1->SetFlexibleDirection(wxBOTH);
+	wx_flex_grid_sizer_2_1->SetNonFlexibleGrowMode(wxFLEX_GROWMODE_SPECIFIED);
 
 	wxBoxSizer* wx_box_sizer_3;
-	wx_box_sizer_3 = new wxBoxSizer(wxVERTICAL);
+	wx_box_sizer_3 = new wxBoxSizer(wxHORIZONTAL);
 
 	for (int i = 0; i < 8; i++)
 	{
 		if (i < 4)
 		{
 			pin_labels[i] = new wxStaticText(this, wxID_ANY, wxString::Format("OUT%i", i), wxDefaultPosition, wxDefaultSize, 0);
-			wx_grid_sizer_1_1_1->Add(pin_labels[i], 0, wxALIGN_CENTER, 0);
+			pin_labels[i]->Wrap(-1);
+			wx_flex_grid_sizer_1_1->Add(pin_labels[i], 0, wxALIGN_CENTER, 0);
 		}
 		else
 		{
 			pin_labels[i] = new wxStaticText(this, wxID_ANY, wxString::Format("IN%i", i-4), wxDefaultPosition, wxDefaultSize, 0);
-			wx_grid_sizer_1_1_2->Add(pin_labels[i], 0, wxALIGN_CENTER, 0);
+			pin_labels[i]->Wrap(-1);
+			wx_flex_grid_sizer_1_2->Add(pin_labels[i], 0, wxALIGN_CENTER, 0);
 		}		
 	}
 
@@ -56,23 +74,39 @@ VirtualMcuMain::VirtualMcuMain() : wxFrame(nullptr, wxID_ANY, "VIRTUAL MCU")
 		output_pin[i]->SetMinSize(wxSize(50, 50));
 		output_pin[i]->SetValue(false);
 		output_pin[i]->Bind(wxEVT_COMMAND_TOGGLEBUTTON_CLICKED, &VirtualMcuMain::OnButtonClick, this);
-		wx_grid_sizer_1_1_1->Add(output_pin[i], 0, wxEXPAND, 0);
+		wx_flex_grid_sizer_1_1->Add(output_pin[i], 0, wxEXPAND, 0);
 		
 
 		input_pin[i] = new wxToggleButton(this, ID_input_0 + i, wxString::Format("LOW", i), wxDefaultPosition, wxDefaultSize, 0);
 		input_pin[i]->SetMinSize(wxSize(50, 50));
 		output_pin[i]->SetValue(false);
 		input_pin[i]->Bind(wxEVT_COMMAND_TOGGLEBUTTON_CLICKED, &VirtualMcuMain::OnButtonClick, this);
-		wx_grid_sizer_1_1_2->Add(input_pin[i], 0, wxEXPAND, 0);
+		wx_flex_grid_sizer_1_2->Add(input_pin[i], 0, wxEXPAND, 0);
 	}	
 
-	wx_box_sizer_1_1->Add(wx_grid_sizer_1_1_1, 1,  wxLEFT, 5);
-	wx_box_sizer_1_1->Add(wx_grid_sizer_1_1_2, 1,  wxLEFT|wxRIGHT, 5);
-	wx_box_sizer_1->Add(wx_box_sizer_1_1, 1, wxEXPAND, 5);
-	wx_box_sizer_1->Add(wx_box_sizer_1_2, 1, wxEXPAND, 5);
-	wx_box_sizer_0->Add(wx_box_sizer_1, 1, wxEXPAND, 5);
-	wx_box_sizer_0->Add(wx_box_sizer_2, 1, wxEXPAND, 5);
-	wx_box_sizer_0->Add(wx_box_sizer_3, 1, wxEXPAND, 5);
+	adc_label_0 = new wxStaticText(this, wxID_ANY, wxT("ADC_CH0"), wxDefaultPosition, wxDefaultSize, 0);
+	adc_label_1 = new wxStaticText(this, wxID_ANY, wxT("ADC_CH1"), wxDefaultPosition, wxDefaultSize, 0);
+	adc_voltage_label_0 = new wxStaticText(this, wxID_ANY, wxT("1.65 V"), wxDefaultPosition, wxDefaultSize, 0);
+	adc_voltage_label_1 = new wxStaticText(this, wxID_ANY, wxT("1.65 V"), wxDefaultPosition, wxDefaultSize, 0);
+	adc_slider_0 = new wxSlider(this, ID_slider_0, 2047, 0, 4095, wxDefaultPosition, wxDefaultSize, wxSL_HORIZONTAL);
+	adc_slider_0->Bind(wxEVT_SLIDER, &VirtualMcuMain::OnSliderUpdate, this);
+	adc_slider_1 = new wxSlider(this, ID_slider_1, 2047, 0, 4095, wxDefaultPosition, wxDefaultSize, wxSL_HORIZONTAL);
+	adc_slider_1->Bind(wxEVT_SLIDER, &VirtualMcuMain::OnSliderUpdate, this);
+
+	wx_flex_grid_sizer_2_1->Add(adc_label_0, 0, wxALIGN_CENTER | wxALL, 5);
+	wx_flex_grid_sizer_2_1->Add(adc_slider_0, 1, wxEXPAND | wxALL, 5);
+	wx_flex_grid_sizer_2_1->Add(adc_voltage_label_0, 0, wxALIGN_CENTER | wxALL, 5);
+	wx_flex_grid_sizer_2_1->Add(adc_label_1, 0, wxALIGN_CENTER | wxALL, 5);
+	wx_flex_grid_sizer_2_1->Add(adc_slider_1, 1, wxEXPAND | wxALL, 5);
+	wx_flex_grid_sizer_2_1->Add(adc_voltage_label_1, 0, wxALIGN_CENTER | wxALL, 5);
+
+	wx_box_sizer_2->Add(wx_flex_grid_sizer_2_1, 1, wxEXPAND | wxRIGHT, 20);
+
+	wx_box_sizer_1->Add(wx_flex_grid_sizer_1_1, 1, wxRIGHT | wxEXPAND, 10);
+	wx_box_sizer_1->Add(wx_flex_grid_sizer_1_2, 1, wxLEFT | wxEXPAND, 10);
+	wx_box_sizer_0->Add(wx_box_sizer_1, 1, wxLEFT | wxRIGHT | wxTOP| wxSHAPED, 10);
+	wx_box_sizer_0->Add(wx_box_sizer_2, 1, wxLEFT | wxRIGHT | wxSHAPED, 10);
+	wx_box_sizer_0->Add(wx_box_sizer_3, 1, wxLEFT | wxRIGHT | wxSHAPED, 10);
 
 	this->SetSizer(wx_box_sizer_0);
 	this->Layout();
@@ -85,7 +119,7 @@ void VirtualMcuMain::OnButtonClick(wxCommandEvent& evt)
 	bool current_button_state = false;
 	wxString current_button_label;
 
-	current_button_ID = evt.GetId() - 8000; // gives 0-7
+	current_button_ID = evt.GetId() - wxID_HIGHEST; // gives 0-7
 
 	if (current_button_ID < 4)
 	{
@@ -98,6 +132,30 @@ void VirtualMcuMain::OnButtonClick(wxCommandEvent& evt)
 		current_button_state = input_pin[current_button_ID-4]->GetValue();
 		current_button_label = ((true == current_button_state) ? "HIGH" : "LOW");
 		input_pin[current_button_ID-4]->SetLabel(current_button_label);
+	}
+	evt.Skip();
+}
+
+
+void VirtualMcuMain::OnSliderUpdate(wxCommandEvent& evt)
+{
+	int current_slider_ID = 0;
+	int slider_value = 0;
+	float voltage = 0.0;
+
+	current_slider_ID = evt.GetId();
+
+	if (ID_slider_0 == current_slider_ID)
+	{
+		slider_value = adc_slider_0->GetValue();
+		voltage = slider_value * (MCU_NOMINAL_VOLTAGE / (ADC_MAX_VALUE + 1));
+		adc_voltage_label_0->SetLabel(wxString::Format("%.2f V", voltage));
+	}
+	else if(ID_slider_1 == current_slider_ID)
+	{
+		slider_value = adc_slider_1->GetValue();
+		voltage = slider_value * (MCU_NOMINAL_VOLTAGE / (ADC_MAX_VALUE + 1));
+		adc_voltage_label_1->SetLabel(wxString::Format("%.2f V", voltage));;
 	}
 	evt.Skip();
 }
--- Virtual MCU GUI/VirtualMcuMain.h
+++ Virtual MCU GUI/VirtualMcuMain.h
@@ -2,29 +2,44 @@
 #include "wx/wx.h"
 #include <wx/tglbtn.h>
 
+#define MCU_NOMINAL_VOLTAGE (3.3f)
+#define ADC_MAX_VALUE (4095u)
+
 class VirtualMcuMain : public wxFrame
 {
-	enum {
-		ID_output_0 = 8000,
-		ID_output_1,
-		ID_output_2,
-		ID_output_3,
-		ID_input_0,
-		ID_input_1,
-		ID_input_2,
-		ID_input_3,
-	};
 public:
 	VirtualMcuMain();
 	~VirtualMcuMain();
 	void OnButtonClick(wxCommandEvent &evt);
+	void OnSliderUpdate(wxCommandEvent& evt);
 
 protected:
 	wxToggleButton* output_pin[4];
 	wxToggleButton* input_pin[4];
 	wxStaticText* pin_labels[8];
+	wxSlider* adc_slider_0;
+	wxSlider* adc_slider_1;
+	wxStaticText* adc_label_0;
+	wxStaticText* adc_label_1;
+	wxStaticText* adc_voltage_label_0;
+	wxStaticText* adc_voltage_label_1;
 
 private:
 	wxDECLARE_EVENT_TABLE();
+	int adc_channel_0_value = 0;
+	int adc_channel_1_value = 0;
 	
+};
+
+enum IDs{
+	ID_output_0 = wxID_HIGHEST,
+	ID_output_1,
+	ID_output_2,
+	ID_output_3,
+	ID_input_0,
+	ID_input_1,
+	ID_input_2,
+	ID_input_3,
+	ID_slider_0,
+	ID_slider_1
 };
