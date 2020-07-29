8c1deac757d663a6ef14f93348afa6c58ea9a83e
Branimir Mihelčić <bran1mir.bm@gmail.com>
2020-07-29 19:01:43 +0200

    Final STM32 Demo App

--- STM32_Demo/Src/main.c
+++ STM32_Demo/Src/main.c
@@ -14,6 +14,7 @@ int main(void)
 {
     uint16_t adcValue1 = 0;
     uint16_t adcValue2 = 0;
+    uint8_t inputState = 0;
 
     BM_HAL_init();
     BM_HAL_GPIO_init();
@@ -26,24 +27,24 @@ int main(void)
         // INPUT0 CHECK
         if(BM_GPIO_LOW == BM_HAL_GPIO_digitalRead(INPUT0_GPIO_Port, INPUT0_Pin))
         {
-            CAN_tx_data[0] |= 1;
+            inputState |= 1;
             BM_HAL_GPIO_digitalWrite(OUTPUT0_GPIO_Port, OUTPUT0_Pin, BM_GPIO_HIGH);
         }
         else if(BM_GPIO_HIGH == BM_HAL_GPIO_digitalRead(INPUT0_GPIO_Port, INPUT0_Pin))
         {
-            CAN_tx_data[0] &=  ~1;
+            inputState &=  ~1;
             BM_HAL_GPIO_digitalWrite(OUTPUT0_GPIO_Port, OUTPUT0_Pin, BM_GPIO_LOW);
         }
 
         // INPUT1 CHECK
         if(BM_GPIO_LOW == BM_HAL_GPIO_digitalRead(INPUT1_GPIO_Port, INPUT1_Pin))
         {
-            CAN_tx_data[0] |= (1 << 1);
+            inputState |= (1 << 1);
             BM_HAL_GPIO_digitalWrite(OUTPUT1_GPIO_Port, OUTPUT1_Pin, BM_GPIO_HIGH);
         }
         else if(BM_GPIO_HIGH == BM_HAL_GPIO_digitalRead(INPUT1_GPIO_Port, INPUT1_Pin))
         {
-            CAN_tx_data[0] &=  ~(1 << 1);
+            inputState &=  ~(1 << 1);
             BM_HAL_GPIO_digitalWrite(OUTPUT1_GPIO_Port, OUTPUT1_Pin, BM_GPIO_LOW);
         }
 
@@ -51,12 +52,12 @@ int main(void)
         // INPUT2 CHECK
         if(BM_GPIO_LOW == BM_HAL_GPIO_digitalRead(INPUT2_GPIO_Port, INPUT2_Pin))
         {
-            CAN_tx_data[0] |= (1 << 2);
+            inputState |= (1 << 2);
             BM_HAL_GPIO_digitalWrite(OUTPUT2_GPIO_Port, OUTPUT2_Pin, BM_GPIO_HIGH);
         }
         else if(BM_GPIO_HIGH == BM_HAL_GPIO_digitalRead(INPUT2_GPIO_Port, INPUT2_Pin))
         {
-            CAN_tx_data[0] &=  ~(1 << 2);
+            inputState &=  ~(1 << 2);
             BM_HAL_GPIO_digitalWrite(OUTPUT2_GPIO_Port, OUTPUT2_Pin, BM_GPIO_LOW);
         }
 
@@ -64,15 +65,16 @@ int main(void)
         // INPUT3 CHECK
         if(BM_GPIO_LOW == BM_HAL_GPIO_digitalRead(INPUT3_GPIO_Port, INPUT3_Pin))
         {
-            CAN_tx_data[0] |= (1 << 3);
+            inputState |= (1 << 3);
             BM_HAL_GPIO_digitalWrite(OUTPUT3_GPIO_Port, OUTPUT3_Pin, BM_GPIO_HIGH);
         }
         else if(BM_GPIO_HIGH == BM_HAL_GPIO_digitalRead(INPUT3_GPIO_Port, INPUT3_Pin))
         {
-            CAN_tx_data[0] &=  ~(1 << 3);
+            inputState &=  ~(1 << 3);
             BM_HAL_GPIO_digitalWrite(OUTPUT3_GPIO_Port, OUTPUT3_Pin, BM_GPIO_LOW);
         }
 
+        CAN_tx_data[0] = inputState;
         adcValue1 = BM_HAL_ADC_readChannel(ADC1, LL_ADC_CHANNEL_0);
         CAN_tx_data[1] = adcValue1;
         CAN_tx_data[2] = (adcValue1 >> 8) & 0x0F;
@@ -82,9 +84,13 @@ int main(void)
 
         BM_HAL_CAN_send(CAN_tx_data, 5);
 
-        HAL_Delay(200);
+        HAL_Delay(500);
 
-        BM_HAL_UART_printf("%d\t%d\t%d\r\n", CAN_tx_data[0], adcValue1, adcValue2);
-
+        BM_HAL_UART_printf("INPUT: %d  ADC0: %d  ADC1: %d\r\n", inputState, adcValue1, adcValue2);
+        for(int i=0; i < 5; i++)
+        {
+            BM_HAL_UART_printf("CAN BYTE %d: 0x%x\r\n", i, CAN_tx_data[i]);
+        }
+        BM_HAL_UART_printf("\n");
    }
 }
